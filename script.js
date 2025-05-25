60370f4c5c98eccb02b2223fe28f3b158884c50e
Abhishek Airan <83405230+abhishekairan@users.noreply.github.com>
2025-05-25 21:32:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
+	str = str.trim();
+
+	
+
+    const spaceIndex = str.indexOf(' ');
+
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+    // Return substring from start to first space
+    return str.slice(0, spaceIndex);
 }
 
 // Do not change the code below
